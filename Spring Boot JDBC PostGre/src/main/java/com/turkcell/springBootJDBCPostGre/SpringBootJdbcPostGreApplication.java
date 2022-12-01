b0954107ca80834c3be04c675dc548b072917395
Ali Erdem Alkoç <alierdemalkoc@gmail.com>
2022-12-01 15:08:47 +0300

    düzeltme

---
+++ Spring Boot JDBC PostGre/src/main/java/com/turkcell/springBootJDBCPostGre/SpringBootJdbcPostGreApplication.java
@@ -0,0 +1,13 @@
+package com.turkcell.springBootJDBCPostGre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcPostGreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcPostGreApplication.class, args);
+	}
+
+}
---
+++ Spring Boot JDBC PostGre/src/test/java/com/turkcell/springBootJDBCPostGre/SpringBootJdbcPostGreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.springBootJDBCPostGre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJdbcPostGreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
